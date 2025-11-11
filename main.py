710411d37aee15e2d1fff8bcd6cc4913cfb0ffa5
Des Whelan <wheland6@tcd.ie>
2025-11-11 11:25:34 +0000

    save entries to file and reset form

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 from tkinter import *
+
+USER_EMAIL_ADDRESS = "wheland6@gmail.com"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    # TODO: stretch - check for values before saving
+    # TODO: stretch - check for existing entry for a given website before saving
+    website = entry_website.get()
+    email_username = entry_email_username.get()
+    password = entry_password.get()
+
+    # TODO: stretch - write to file outside project
+    with open("data.txt", mode="a") as data_file:
+        data_file.write(f"{website} | {email_username} | {password}\n")
+
+    reset_form()
+
+def reset_form():
+    entry_website.delete(0, "end")
+    entry_email_username.delete(0, "end")
+    entry_password.delete(0, "end")
+    entry_email_username.insert(0, USER_EMAIL_ADDRESS)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 LOGO_IMAGE = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=LOGO_IMAGE)
+canvas.create_image(85, 100, image=LOGO_IMAGE)
 canvas.grid(column=2, row=1)
-
 
 label_website = Label(text="Website:")
 label_website.grid(column=1, row=2, sticky=E)
@@ -24,16 +43,17 @@ label_password = Label(text="Password:")
 label_password.grid(column=1, row=4, sticky=E)
 
 entry_website = Entry(width=45)
+entry_website.focus()
 entry_website.grid(column=2, row=2, columnspan=2)
 
 entry_email_username = Entry(width=45)
-entry_email_username.insert(0, "wheland6@gmail.com")
+entry_email_username.insert(0, USER_EMAIL_ADDRESS)
 entry_email_username.grid(column=2, row=3, columnspan=2)
 
 entry_password = Entry(width=35)
 entry_password.grid(column=2, row=4)
 
-button_add = Button(width=38, text="Add")
+button_add = Button(width=38, text="Add", command=save)
 button_add.grid(column=2, row=5, columnspan=2)
 
 button_generate_password = Button(text="Generate")
